7b66c59967a82a732740a1d9fa824162345eb035
nxnaxx <76546167+nxnaxx@users.noreply.github.com>
2021-01-08 22:43:26 +0900

    Add files via upload

---
+++ nxnaxx.github.io-main/ex6-11.js
@@ -0,0 +1,13 @@
+id=prompt('아이디 입력');
+if(id=='admin'){
+    password=prompt('비밀번호 입력');
+    if(password==='12345'){
+        location.href="http://www.google.com"
+    }
+    else {
+        alert("저리가!!!");
+    }
+}
+else {
+    alert("저리가!!!");
+}
